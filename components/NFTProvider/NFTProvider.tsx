0b05cd7b8e153ed6642f99a8ab9e398ff94165bd
Wojciech Lubojanski <lubojanski.wojciech@gmail.com>
2023-05-20 12:32:58 +0200

    remove hardcoded wallet address

--- components/NFTProvider/NFTProvider.tsx
+++ components/NFTProvider/NFTProvider.tsx
@@ -27,7 +27,7 @@ const NFTProvider = ({ children }: { children: ReactNode }) => {
     queryFn: async () => {
       const res = await fetch(
         // TODO: use env variable
-        `https://eth-mainnet.g.alchemy.com/nft/v3/${process.env.NEXT_PUBLIC_ALCHEMY_ID}/getNFTsForOwner?owner=0x154B4045F07B48C3B75D73a3f6C7C11Dfec95b4a&withMetadata=true&pageSize=100`
+        `https://eth-mainnet.g.alchemy.com/nft/v3/${process.env.NEXT_PUBLIC_ALCHEMY_ID}/getNFTsForOwner?owner=${address}&withMetadata=true&pageSize=100`
       );
       const json: ApiResponse = await res.json();
       const filteredNftsWithPosition = json.ownedNfts.reduce((acc, nft) => {
